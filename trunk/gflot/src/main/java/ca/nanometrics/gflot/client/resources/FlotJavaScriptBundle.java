59d9d0215126b6416db6f5b6280f6695fe223855
nmr.morel@gmail.com <nmr.morel@gmail.com@80f5873d-66b9-501f-4f9f-694fd2079b93>
2011-12-10 16:38:29 +0000

    added automatic injection of flot javascript files

---
+++ trunk/gflot/src/main/java/ca/nanometrics/gflot/client/resources/FlotJavaScriptBundle.java
@@ -0,0 +1,20 @@
+package ca.nanometrics.gflot.client.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.TextResource;
+
+public interface FlotJavaScriptBundle
+    extends ClientBundle
+{
+    @Source( "jquery.min.js" )
+    TextResource jquery();
+
+    @Source( "jquery.flot.min.js" )
+    TextResource flot();
+
+    @Source( "jquery.flot.selection.min.js" )
+    TextResource flotSelection();
+
+    @Source( "excanvas.min.js" )
+    TextResource excanvas();
+}
