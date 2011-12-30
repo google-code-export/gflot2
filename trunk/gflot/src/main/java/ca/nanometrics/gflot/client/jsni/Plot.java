b3a6bd81a1d4c8851493f1f8f7c7a147a0f34479
nmr.morel@gmail.com <nmr.morel@gmail.com@80f5873d-66b9-501f-4f9f-694fd2079b93>
2011-12-30 15:13:55 +0000

    add jsni methods to retrieve plot options after the creation of plot

--- trunk/gflot/src/main/java/ca/nanometrics/gflot/client/jsni/Plot.java
+++ trunk/gflot/src/main/java/ca/nanometrics/gflot/client/jsni/Plot.java
@@ -29,6 +29,7 @@ import ca.nanometrics.gflot.client.options.PlotOptions;
 import ca.nanometrics.gflot.client.util.JSONHelper;
 
 import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.Element;
 
 /**
@@ -119,5 +120,10 @@ public class Plot
     public final int getPlotOffsetBottom()
     {
         return PlotImpl.getPlotOffsetBottom( this );
+    }
+
+    public final PlotOptions getPlotOptions()
+    {
+        return new PlotOptions(new JSONObject(PlotImpl.getPlotOptions( this )));
     }
 }
