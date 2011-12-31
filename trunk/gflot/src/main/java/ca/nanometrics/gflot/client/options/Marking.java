1fc69d57b00437246ebbe45a6c2e02dbc026b335
nmr.morel@gmail.com <nmr.morel@gmail.com@80f5873d-66b9-501f-4f9f-694fd2079b93>
2011-12-31 18:19:34 +0000

    add get methods on gridoptions and markings + constructor with JSONObject + new methods to set linewidth and colors on markings

--- trunk/gflot/src/main/java/ca/nanometrics/gflot/client/options/Marking.java
+++ trunk/gflot/src/main/java/ca/nanometrics/gflot/client/options/Marking.java
@@ -2,23 +2,91 @@ package ca.nanometrics.gflot.client.options;
 
 import ca.nanometrics.gflot.client.util.JSONObjectWrapper;
 
+import com.google.gwt.json.client.JSONObject;
+
 /**
- * 
  * @author Mohamed M. El-Kalioby
  * @since Septmeber 21, 2009
  */
-public class Marking extends JSONObjectWrapper {
+public class Marking
+    extends JSONObjectWrapper
+{
+    private static final String X_AXIS_KEY = "xaxis";
 
-	public void setY(Range Y) {
-		put("yaxis", Y);
-	}
+    private static final String Y_AXIS_KEY = "yaxis";
 
-	public void setX(Range X) {
-		put("xaxis", X);
-	}
+    private static final String COLOR_KEY = "color";
 
-	public void setColor(String cssColor) {
-		put("color", cssColor);
-	}
+    private static final String LINE_WIDTH_KEY = "lineWidth";
+
+    public Marking()
+    {
+        super();
+    }
+
+    public Marking( JSONObject jsonObj )
+    {
+        super( jsonObj );
+    }
+
+    public Marking setX( Range X )
+    {
+        put( X_AXIS_KEY, X );
+        return this;
+    }
+
+    public Range getX()
+    {
+        JSONObject obj = getObject( X_AXIS_KEY );
+        if ( null == obj )
+        {
+            return null;
+        }
+        else
+        {
+            return new Range( obj );
+        }
+    }
+
+    public Marking setY( Range Y )
+    {
+        put( Y_AXIS_KEY, Y );
+        return this;
+    }
+
+    public Range getY()
+    {
+        JSONObject obj = getObject( Y_AXIS_KEY );
+        if ( null == obj )
+        {
+            return null;
+        }
+        else
+        {
+            return new Range( obj );
+        }
+    }
+
+    public Marking setColor( String color )
+    {
+        put( COLOR_KEY, color );
+        return this;
+    }
+
+    public String getColor()
+    {
+        return getString( COLOR_KEY );
+    }
+
+    public Marking setLineWidth( int lineWidth )
+    {
+        put( LINE_WIDTH_KEY, lineWidth );
+        return this;
+    }
+
+    public Integer getLineWidth()
+    {
+        return getInteger( LINE_WIDTH_KEY );
+    }
 
 }
